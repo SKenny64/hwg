e8b62de392ec9f4780c7f335b1c969aa7a1dc89a
Alexis Grivel <alexis.grivel@live.fr>
2024-03-06 12:01:32 +0100

    création de l'entité 'reservation'

--- migrations/Version20240306110112.php
+++ migrations/Version20240306110112.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240306103925 extends AbstractMigration
+final class Version20240306110112 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20240306103925 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE participation (id INT AUTO_INCREMENT NOT NULL, date_participation DATE DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE reservation (id INT AUTO_INCREMENT NOT NULL, date_reservation DATE DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE participation');
+        $this->addSql('DROP TABLE reservation');
     }
 }
