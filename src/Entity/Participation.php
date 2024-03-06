bea835fb3c986837a26caeca60d820192eb6fed6
Alexis Grivel <alexis.grivel@live.fr>
2024-03-06 11:39:55 +0100

    création entité participation

---
+++ src/Entity/Participation.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ParticipationRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ParticipationRepository::class)]
+class Participation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $dateParticipation = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateParticipation(): ?\DateTimeInterface
+    {
+        return $this->dateParticipation;
+    }
+
+    public function setDateParticipation(?\DateTimeInterface $dateParticipation): static
+    {
+        $this->dateParticipation = $dateParticipation;
+
+        return $this;
+    }
+}
