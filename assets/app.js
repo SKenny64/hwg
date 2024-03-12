33affa63568269e99720c8b8bce9dde6be5444d0
Alexis Grivel <alexis.grivel@live.fr>
2024-03-12 22:20:00 +0100

    creation du crud par la participation et début de la page "dashboard"

--- assets/app.js
+++ assets/app.js
@@ -12,12 +12,12 @@ import './styles/app.scss';
 require('bootstrap');
 
 // or you can include specific pieces
-// require('bootstrap/js/dist/tooltip');
-// require('bootstrap/js/dist/popover');
+require('bootstrap/js/dist/tooltip');
+require('bootstrap/js/dist/popover');
 
-$(document).ready(function () {
-    $('[data-toggle="popover"]').popover();
-});
+// $(document).ready(function () {
+//     $('[data-toggle="popover"]').popover();
+// });
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
 
